4eff566302bbb98711d6844e8eb826b0d10bf54d
Софья <sonyavyatkinaa@gmail.com>
2024-06-10 20:43:22 +0500

    4.15. Шаблонизируй то

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
 import ListPresenter from './presenter/list-presenter.js';
 import PointsModel from './model/points-model.js';
 import {render} from './framework/render.js';
 
 const filterContainer = document.querySelector('.trip-controls__filters');
-const pointsContainer = document.querySelector('.trip-events');
+const pointsListContainer = document.querySelector('.trip-events');
 
 const pointsModel = new PointsModel();
 const listPresenter = new ListPresenter({
-  container: pointsContainer,
+  container: pointsListContainer,
   pointsModel
 });
 
 render(new FilterView(), filterContainer);
-render(new SortView(), pointsContainer);
 listPresenter.init();
