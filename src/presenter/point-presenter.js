3702669a2222b82457d9772cb254dd68c9e3bf26
Софья <sonyavyatkinaa@gmail.com>
2024-06-20 01:52:00 +0500

    6.7. Обновление века (часть 2)

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -107,6 +107,7 @@ export default class PointPresenter {
   resetView() {
     if (this.#mode !== Mode.DEFAULT) {
       this.#replaceEditFormToPoint();
+      this.#pointEditComponent.reset(this.#point);
     }
   }
 
